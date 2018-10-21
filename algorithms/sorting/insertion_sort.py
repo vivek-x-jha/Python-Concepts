1de24578697d9329b8375167bbd21c5cf4c1a722
Vivek Jha <vivek.x.jha@gmail.com>
2018-10-20 23:46:33 -0500

    completed insertion sort algorithm

--- algorithms/sorting/insertion_sort.py
+++ algorithms/sorting/insertion_sort.py
@@ -8,9 +8,10 @@ def insertionSort(array):
 	for j in range(1, len(array)):
 		key = array[j]
 		i = j - 1
-		while i > 1 and array[i] > key:
-			pass
-
+		while i >= 0 and array[i] > key:
+			array[i + 1] = array[i]
+			i = i - 1
+		array[i + 1] = key
 
 def main():
 	a = [1, 4, 0, -3, 2, 7]
