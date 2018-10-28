de07ea716c9b943619c7016cc277e4d7fd511431
Vivek Jha <vivek.x.jha@gmail.com>
2018-10-28 02:42:39 -0500

    changed data folder name

--- datacamp/pandas_prac/world_university_rankings.py
+++ datacamp/pandas_prac/world_university_rankings.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 # Import Times Higher Education World University Rankings data
-times_df = pd.read_csv('csvdata/timesData.csv', thousands=",")
+times_df = pd.read_csv('data/timesData.csv', thousands=",")
 
 # Import Academic Ranking of World Universities data
-shanghai_df = pd.read_csv('csvdata/shanghaiData.csv')
+shanghai_df = pd.read_csv('data/shanghaiData.csv')
 
 # Return the first rows of `times_df`
 for dataframe in times_df, shanghai_df:
