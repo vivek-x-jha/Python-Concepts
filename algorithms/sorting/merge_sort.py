a041c7eeda0f6ee2cf71ae62640cbaf38808fe32
Vivek Jha <vivek.x.jha@gmail.com>
2018-10-31 16:50:15 -0500

    Added Merge Sort script

---
+++ algorithms/sorting/merge_sort.py
@@ -0,0 +1,23 @@
+def insertionSort(array):
+	"""Sorting algorithm that compares each element and orders inplace
+
+	Time Complexity: O(n), Ω(n)
+	Space Complexity: O(n)
+	"""
+
+	for j in range(1, len(array)):
+		key = array[j]
+		i = j - 1
+		while i >= 0 and array[i] > key:
+			array[i + 1] = array[i]
+			i -= 1
+		array[i + 1] = key
+
+def main():
+	a = [1, 4, 0, -3, 2, 7]
+	insertionSort(a)
+	print(a)
+
+
+if __name__ == '__main__':
+    main()
