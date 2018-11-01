dcdf1c3a80372ef27562a6f6dd59e348d713a93d
Vivek Jha <vivek.x.jha@gmail.com>
2018-10-31 22:38:27 -0500

    Added append, length, & repr to LinkedList implementation

--- DataStrucsAlgos/02_DataStructures/LinkedLists.py
+++ DataStrucsAlgos/02_DataStructures/LinkedLists.py
@@ -10,6 +10,29 @@ class Node:
 class Linkedlist:
     def __init__(self):
         self.head = Node()
-        self.next = None
+        self.tail = self.head
+        self.length = 0
 
     def append(self, value):
+        new_node = Node(value)
+        self.tail.next = new_node
+        self.tail = new_node
+        self.length += 1
+
+    def __repr__(self):
+        values = []
+        cur_node = self.head
+        while cur_node.next is not None:
+            cur_node = cur_node.next
+            values.append(cur_node.value)
+        return str(values)
+
+    def __len__(self):
+        return self.length
+
+
+ll = Linkedlist()
+ll.append(-3)
+ll.append(1)
+print(len(ll))
+print(ll)
