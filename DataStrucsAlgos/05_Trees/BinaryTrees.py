cef029161562f6db19e0fa74d3edd2dbc9814f9c
Vivek Jha <vivek.x.jha@gmail.com>
2018-11-23 09:57:37 -0800

    Created Binary Tree class

---
+++ DataStrucsAlgos/05_Trees/BinaryTrees.py
@@ -0,0 +1,10 @@
+class BinaryTree:
+    """Insert docstring
+    """
+
+    class _Node:
+        def __init__(self, value):
+            self._value = value
+            self._left = None
+            self._right = None
+
